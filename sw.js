f0fb3a7e5a54bddf7cca86f41c97a8164b80d03c
rickmrweb <83184078+rickmrweb@users.noreply.github.com>
2025-01-15 12:28:55 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Espera 11 minutos después de hacer los cambios en tu sitio, para después
  * actualizar este archivo. */
-const CACHE = "rickmrweb-1.2.9";
+const CACHE = "rickmrweb-1.2.10";
 
 /** Archivos requeridos para que la aplicación funcione fuera de línea. */
 const ARCHIVOS = [
